4ff1e86cd5773632528dd35c6f5691964eb3b405
Christopher <christopher.rydberg@iths.se>
2024-10-22 20:52:11 +0200

    maintenance

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  const url = 'https://render-testing-backend.onrender.com/';
-  const data = fetch(url);
-  return <>Testar att hämta från backend: {data}</>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch('https://render-testing-backend.onrender.com/')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setData(data);
+      });
+  }, []);
+  return (
+    <div>
+      <h1>Data från backend</h1>
+      <pre>{data && JSON.stringify(data)}</pre>
+    </div>
+  );
 }
 
 export default App;
