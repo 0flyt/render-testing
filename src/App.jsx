7a3911f219463314b9b8f82ba3498480f6b63713
Christopher <christopher.rydberg@iths.se>
2024-10-22 14:36:02 +0200

    Added backend fetch

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.css';
 
 function App() {
-  return <>Rent och fint. Fungerar Deploy-hook? Tror det</>;
+  const url = 'https://render-testing-backend.onrender.com/';
+  const data = fetch(url);
+  return (
+    <>
+      <p>Testar att hämta från backend: {data}</p>
+    </>
+  );
 }
 
 export default App;
